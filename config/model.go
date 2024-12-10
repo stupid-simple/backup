64c255453a913ad50b2c995a7d25d66ae011d9b3
i-segura <github@m.isu101.com>
2024-12-10 20:41:37 +0100

    feat!: rename json config archive max file size

--- config/model.go
+++ config/model.go
@@ -9,9 +9,9 @@ type Config struct {
 type ConfigSource struct {
 	SourceDir                string       `json:"source_dir"`
 	ArchiveDir               string       `json:"archive_dir"`
-	ArchivePrefix            string       `json:"archive_prefix"`
-	ArchiveMaxFileSize       SizeArgument `json:"archive_max_file_size"`
-	ArchiveIncludeLargeFiles bool         `json:"archive_include_large_files"`
+	ArchivePrefix            string       `json:"archive_prefix,omitempty"`
+	ArchiveMaxFileSize       SizeArgument `json:"archive_max_sum_size,omitempty"`
+	ArchiveIncludeLargeFiles bool         `json:"archive_include_large_files,omitempty"`
 	Enable                   bool         `json:"enable"`
 	Schedule                 string       `json:"cron"`
 }
@@ -19,9 +19,14 @@ type ConfigSource struct {
 func (s ConfigSource) MarshalZerologObject(e *zerolog.Event) {
 	e.Str("source_dir", s.SourceDir)
 	e.Str("archive_dir", s.ArchiveDir)
-	e.Str("archive_prefix", s.ArchivePrefix)
-	e.Int64("archive_max_file_size", s.ArchiveMaxFileSize.Size)
-	e.Bool("archive_include_large_files", s.ArchiveIncludeLargeFiles)
 	e.Bool("enable", s.Enable)
 	e.Str("schedule", s.Schedule)
+
+	if s.ArchivePrefix != "" {
+		e.Str("archive_prefix", s.ArchivePrefix)
+	}
+	if s.ArchiveMaxFileSize.Size > 0 {
+		e.Int64("archive_max_sum_size", s.ArchiveMaxFileSize.Size)
+		e.Bool("archive_include_large_files", s.ArchiveIncludeLargeFiles)
+	}
 }
