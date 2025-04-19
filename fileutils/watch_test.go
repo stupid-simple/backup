d1a15077cbba0470078b3be4d6e3fc8849544939
i-segura <github@m.isu101.com>
2025-04-19 14:59:58 +0200

    test(fileutils): WatchFile CI race condition

--- fileutils/watch_test.go
+++ fileutils/watch_test.go
@@ -47,17 +47,27 @@ func TestWatchFile_Changed(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// Create a channel to collect errors from the watcher
+	errCh := make(chan error, 1)
+
 	notify := make(chan struct{})
 	defer close(notify)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	watcher, err := fileutils.WatchFile(ctx, testPath, notify, func(err error) {
-		t.Fatal(err)
+		select {
+		case errCh <- err:
+		default:
+		}
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Make sure file is fully written before continuing.
+	time.Sleep(100 * time.Millisecond)
 
 	f, err := os.OpenFile(testPath, os.O_WRONLY|os.O_APPEND, 0600)
 	if err != nil {
@@ -69,14 +79,36 @@ func TestWatchFile_Changed(t *testing.T) {
 		_ = f.Close()
 		t.Fatal(err)
 	}
-	_ = f.Close()
 
+	err = f.Sync() // Ensure data is written to disk.
+	if err != nil {
+		_ = f.Close()
+		t.Fatal(err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Make sure file changes are visible before notifying.
+	time.Sleep(100 * time.Millisecond)
+
+	// Signal the watcher to check the file.
 	notify <- struct{}{}
 
+	// Check for errors from the watcher.
+	select {
+	case err := <-errCh:
+		t.Fatal(err)
+	default:
+	}
+
+	// Wait for file change notification.
 	select {
 	case <-watcher:
-		// ok
-	case <-time.After(1 * time.Second):
-		t.Errorf("expected change")
+		// ok - change detected.
+	case <-time.After(2 * time.Second):
+		t.Errorf("expected change but timed out")
 	}
 }
